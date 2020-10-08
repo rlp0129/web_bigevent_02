e6d5ac4f6dd95e468dd0bbee8dc1acacce0c365d
rlp <rlp@rlp0129.cn>
2020-10-08 15:48:07 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,8 +4,30 @@ var baseURL = 'http://ajax.frontend.itheima.net';
 // var baseURL = 'http://ajax.frontend.itheima.net';
 // 生产环境服务器地址
 // var baseURL = 'http://ajax.frontend.itheima.net';
+//1.拦截所有ajax请求
 $.ajaxPrefilter(function(options) {
-    alert(options.url)
+    // alert(options.url)
     options.url = baseURL + options.url;
-    alert(options.url)
+    // alert(options.url)
+
+
+    //2.为请求为/my/开头的所有ajax，配置头信息
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    //3.登录拦截
+    options.complete = function(res) {
+        //complete: function(res) {
+        //console.log(res);
+        //在complete回调函数中，可以使用res.response.JSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //1.删除本地存储中的token
+            localStorage.removeItem('token');
+            //2.重新跳转登录页
+            location.href = "/login.html";
+        }
+    }
+}
 })
